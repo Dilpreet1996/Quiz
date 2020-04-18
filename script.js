1ff4f4cc970fd3233a3e93e8ba21ea906c27c437
Dilpreet1996 <63905690+Dilpreet1996@users.noreply.github.com>
2020-04-18 17:10:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ window.onload=()=>
     
     document.getElementById("btn").style.visibility="hidden";
 }
+document.onkeydown=function(e)
+ {
+     if(event.keyCode==123)
+     {
+         return false;
+     }
+     if(e.ctrlKey && e.shiftKey && e.keyCode=='I'.charCodeAt(0))
+     {
+         return false;
+     }
+     if(e.ctrlKey && e.shiftKey && e.keyCode=='J'.charCodeAt(0))
+     {
+         return false;
+
+     }
+     if(e.ctrlKey &&  e.keyCode=='U'.charCodeAt(0))
+     {
+         return false;
+         
+     }
+ }
 
 const questions=[
     
